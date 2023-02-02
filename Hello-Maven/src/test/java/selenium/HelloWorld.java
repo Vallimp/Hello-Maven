e17e480a4c2d1c46be58bcacdc2116a17255a99e
janga <vallimp@github.com>
2023-02-02 15:45:00 -0500

    changed hello world

--- Hello-Maven/src/test/java/selenium/HelloWorld.java
+++ Hello-Maven/src/test/java/selenium/HelloWorld.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HelloWorld {
+public class HelloWorld 
+{
 	
 	public static void main (String[] args) {
 		
